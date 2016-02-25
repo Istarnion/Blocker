f0643af02174230176c54808dde9aefb855c67f8
Istarnion <hallgeir@loekken.org>
2016-02-25 13:56:23 +0100

    Initial commit

---
+++ desktop/src/com/holypoly/blocker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.holypoly.blocker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.holypoly.blocker.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
